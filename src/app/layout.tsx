78462579993014150b8bd476786ffdf43f62c487
Makhtar <makhtar2gsm@gmail.com>
2026-02-15 11:44:35 +0000

    fixing layout error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -39,10 +39,7 @@ export default function RootLayout({
             <AudioProvider>
               <AppPWAProvider>
                 <div className="flex h-screen text-gray-900 dark:text-gray-100">
-                  <Sidebar
-                    isMobileSidebarOpen={isMobileSidebarOpen}
-                    setIsMobileSidebarOpen={setIsMobileSidebarOpen}
-                  />
+                  <Sidebar />
                   <div className="flex-1 flex flex-col overflow-hidden">
                     <Navbar />
                     <MainContent>
